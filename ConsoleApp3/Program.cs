7e2ec13e0c391aaea620bc0652497dcb220463cb
Denisov-Egor <programming2028@gmail.com>
2026-05-02 20:02:34 +0300

    3

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,34 @@
+﻿class  Person
+{
+  public string Name;
+  public int Age;
+
+  public Person(string name, int age)
+  {
+    Name = name;
+    Age = age;
+  }
+
+  public void DisplayInfo()
+  {
+    Console.WriteLine($"Имя: {Name}, Возраст: {Age}");
+  }
+}
+
+class Program
+{
+  static void Main()
+  {
+    List<Person> people = new List<Person>
+    {
+      new Person("dsds", 19),
+      new Person("dsdadads", 20),
+      new Person("dsdsdd", 12),
+    };
+
+    foreach (Person p in people)
+    {
+      p.DisplayInfo();
+    }
+  }
+}
