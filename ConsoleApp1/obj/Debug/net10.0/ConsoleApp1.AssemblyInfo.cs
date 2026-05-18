735f9d2516a5acb72be4d0d21248f0172ed95b30
Denisov-Egor <programming2028@gmail.com>
2026-05-18 07:58:57 +0300

    10

--- ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+766f1d1680b8aed49ad62b9fe41909948a9be62c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb0bf9e1030b165b87b386fcc9c7ed45136bdcb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,59 @@
+﻿class User
+{
+  public int ID {get; set;}
+  public string Name {get; set;}
+  public int Age {get; set;}
+
+  public User (int id, string name, int age)
+  {
+    if (age < 18)
+    {
+      Console.WriteLine("Возраст не может быть меньше 18 лет.");
+      Environment.Exit(1);
+    }
+    ID = id;
+    Name = name;
+    Age = age;
+  }
+
+  public void DisplayInfo()
+  {
+    Console.WriteLine($"ID {ID}, Имя {Name}, Возраст {Age}");
+  }
+}
+
+class Program
+{
+  static void Main()
+  {
+    User[] users = new User[3];
+
+    for (int i = 0; i < users.Length; i++)
+    {
+      Console.WriteLine("Введите ID ");
+      int ID = Convert.ToInt32(Console.ReadLine());
+
+      Console.WriteLine("Введите Имя ");
+      string Name = Console.ReadLine();
+      
+      Console.WriteLine("Введите возраст ");
+      int Age = Convert.ToInt32(Console.ReadLine());
+      
+      users[i] = new User(ID, Name, Age);
+    }
+
+  List<User> olderUsers = new List<User>();
+
+    Console.WriteLine("Больше 18 лет:");
+    foreach (var user in olderUsers)
+    {
+      user.DisplayInfo();
+    }
+    double averageAge = 0;
+    if (olderUsers.Count > 0)
+    {
+      averageAge = olderUsers.Average(u => u.Age);
+    }
+    Console.WriteLine($"Средний возраст пользователей 18: {averageAge}");
+  }
+}
