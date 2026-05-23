0d310b61e3134e648372d0c79f86ada56b4d342a
Denisov-Egor <programming2028@gmail.com>
2026-05-23 13:15:48 +0300

    12

--- ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+735f9d2516a5acb72be4d0d21248f0172ed95b30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d9ee3e9c53437bf425a11325289bc012e94fb8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,73 @@
+﻿/*
+Создать следубщее классы автомобиль Car гараж Garage мойка Washer
+гараж это коллекция автомобилей
+мойка - назависимое предприятие которое может только мыть автомобил методом мойка
+необходимо делегировать помывку всех автомобилей этому предприятию
+*/
+
+public class Car
+{
+  public string Make;
+  public string Model;
+
+  public override string ToString()
+  {
+    return $"{Make} {Model}";
+  }
+}
+
+public class Garage
+{
+  public List<Car> cars;
+  public Garage()
+  {
+    cars = new List<Car>();
+  }
+  public void AddCar(Car car)
+  {
+    cars.Add(car);
+  }
+  public void RemoveCar(Car car)
+  {
+    cars.Remove(car);
+  }
+  public List<Car> GetCars()
+  {
+    return cars;
+  }
+}
+
+public class Washer
+{
+  private Garage garage;
+  public Washer(Garage garage)
+  {
+    this.garage = garage;
+  }
+  public void WashAllCars()
+  {
+    List<Car> cars = garage.GetCars();
+    foreach (var car in cars)
+    {
+      WashCar(car);
+    }
+  }
+  public void WashCar(Car car)
+  {
+    Console.WriteLine($"Мойка {car}");
+  }
+}
+
+class Program()
+{
+  static void Main()
+  {
+    Garage garage = new Garage();
+    Washer washer = new Washer(garage);
+
+    garage.AddCar(new Car { Make = "Toyota", Model = "Corolla" });
+    garage.AddCar(new Car { Make = "Ford", Model = "Mustang" });
+
+    washer.WashAllCars();
+  }
+}
