9ef72a6e801e994743c3327038b74f65eb5b144b
Denisov-Egor <programming2028@gmail.com>
2026-06-15 08:24:25 +0300

    22

--- ConsoleApp6/obj/Debug/net10.0/ConsoleApp6.AssemblyInfo.cs
+++ ConsoleApp6/obj/Debug/net10.0/ConsoleApp6.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc52f6dfffed426843bc33d1575d52311d7de45a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a78f97527c42a90a3f428e5e813416da1e68fd82")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
