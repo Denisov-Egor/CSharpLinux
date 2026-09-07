b0ea54b445c4623cb7575f2e8d5cfa879ace5851
Denisov-Egor <programming2028@gmail.com>
2026-09-07 21:25:45 +0300

    31

--- ConsoleApp7/obj/Debug/net10.0/ConsoleApp7.AssemblyInfo.cs
+++ ConsoleApp7/obj/Debug/net10.0/ConsoleApp7.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+523aeec9111ada378bb206a2d75cc74fe768fc29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69274540b3eba7fbd3e8b8a020c9a23e7929f3e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
