69274540b3eba7fbd3e8b8a020c9a23e7929f3e7
Denisov-Egor <programming2028@gmail.com>
2026-09-07 14:31:54 +0300

    30

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+  static void Main()
+  {
+    int count = 0;
+
+    int[] arr = new int[16];
+
+    Random rand = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+      arr[i] = rand.Next(0, 2);
+
+      if (arr[i] == 1)
+      {
+        count++;
+      }
+    }
+
+  Console.WriteLine(string.Join(' ', arr));
+  Console.WriteLine(count);
+  }
+}
