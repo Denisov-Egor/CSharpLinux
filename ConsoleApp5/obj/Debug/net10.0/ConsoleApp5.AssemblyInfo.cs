eef96dcad20a35a1dfadb9238786cc2bc734370e
Egor Denisov <programming2028@gmail.com>
2026-09-19 18:01:18 +0300

    36

--- ConsoleApp5/obj/Debug/net10.0/ConsoleApp5.AssemblyInfo.cs
+++ ConsoleApp5/obj/Debug/net10.0/ConsoleApp5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02593f0646db76994e819653d2a93c9809a27c3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd555dfcfdc40d28cb5320310c112c485b9536b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
